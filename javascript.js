873710740251c010e69c9b8c413a27b151a00e49
Pegahz <zahra.javadipour1@gmail.com>
2023-07-31 02:40:07 -0700

    Add buttons and display

--- javascript.js
+++ javascript.js
@@ -1,3 +1,22 @@
+let number1, number2, operator;
+
+function operate(number1, number2, operator) {
+    switch(operator) {
+        case '+':
+            add(number1, number2);
+            break;
+        case '-':
+            subtract(number1, number2);
+            break;
+        case '*':
+            multiply(number1, number2);
+            break;
+        case '/':
+            divide(number1, number2);
+            break;
+    }
+}
+
 function add(a, b) {
     return a + b;
 }
